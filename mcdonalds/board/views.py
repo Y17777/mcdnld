a69225a1884a265942863a469fa5cae6b53958bb
Y17777 <kongo435@yandex.ru>
2024-01-10 20:25:17 +0300

    10/01/23

---
+++ mcdonalds/board/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.views import View
+from .tasks import hello
+
+
+class IndexView(View):
+    def get(self, request):
+        hello.delay()
+        return HttpResponse('Hello!')
